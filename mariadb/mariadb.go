4769de04f7d7ee6b5e757a9c9b60a7c3c73cb705
macadrich <adriel.artiza@gmail.com>
2022-01-30 11:40:43 +0800

    update db connection param

--- mariadb/mariadb.go
+++ mariadb/mariadb.go
@@ -2,7 +2,6 @@ package mariadb
 
 import (
 	"fmt"
-	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -12,13 +11,8 @@ type Database struct {
 	DB *gorm.DB
 }
 
-func NewDatabase() Database {
-	USER := os.Getenv("DB_USER")
-	PASS := os.Getenv("DB_PASSWORD")
-	HOST := os.Getenv("DB_HOST")
-	DBNAME := os.Getenv("DB_NAME")
-
-	URL := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", USER, PASS, HOST, DBNAME)
+func NewDatabase(user, pass, host, dbname string) Database {
+	URL := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", user, pass, host, dbname)
 	fmt.Println(URL)
 	db, err := gorm.Open(mysql.Open(URL))
 
